43e13a9bae8e9860b74f075382de85e0b1f12a27
Kamil Bednarek <kamil.bednarek.2001@gmail.com>
2022-10-22 19:02:08 +0200

    Generic Algorithm without selection

--- task2/src/main/java/pl/meta/backend/Backpack.java
+++ task2/src/main/java/pl/meta/backend/Backpack.java
@@ -1,6 +1,7 @@
 package pl.meta.backend;
 
 import java.util.List;
+import java.util.Random;
 
 public class Backpack {
     private static final int MIN_POSSIBLE_VALUE = 1;
@@ -12,13 +13,16 @@ public class Backpack {
         this.max_weight = max_weight;
         this.possible_items = possible_items;
         this.items_occurrence = new boolean[possible_items.size()];
+        for (int i = 0; i < possible_items.size(); i++) {
+            this.items_occurrence[i] = Math.round(Math.random()) == 0;
+        }
     }
 
-    public int getMax_weight() {
+    public int getMaxWeight() {
         return max_weight;
     }
 
-    public int get_weight() {
+    public int getWeight() {
         int weight = 0, i = 0;
         for (Item item : possible_items) {
             if (items_occurrence[i]) {
@@ -29,8 +33,8 @@ public class Backpack {
         return weight;
     }
 
-    public int get_value() {
-        if (get_weight() > getMax_weight()) {
+    public int getValue() {
+        if (getWeight() > getMaxWeight()) {
             return MIN_POSSIBLE_VALUE;
         }
         int value = 0, i = 0;
@@ -41,5 +45,17 @@ public class Backpack {
             i++;
         }
         return value;
+    }
+
+    public List<Item> getPossible_items() {
+        return possible_items;
+    }
+
+    public boolean[] getItemsOccurrence() {
+        return items_occurrence;
+    }
+
+    public void setItemOccurrence(int index, boolean value) {
+        items_occurrence[index] = value;
     }
 }
---
+++ task2/src/main/java/pl/meta/backend/GenericAlgorithm.java
@@ -0,0 +1,63 @@
+package pl.meta.backend;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericAlgorithm {
+    private final List<Item> items;
+    private final int backpackMaxWeight;
+    private final int populationSize;
+
+    private final int selectionMethod;
+
+    private final int crossMethod;
+
+    public GenericAlgorithm(List<Item> items, int backpackMaxWeight, int populationSize, int selectionMethod,
+                            int crossMethod) {
+        this.items = items;
+        this.backpackMaxWeight = backpackMaxWeight;
+        this.populationSize = populationSize;
+        this.selectionMethod = selectionMethod;
+        this.crossMethod = crossMethod;
+    }
+
+    public List<Backpack> initPopulation() {
+        List<Backpack> population = new ArrayList<>();
+        for (int i = 0; i < populationSize; i++) {
+            population.add(new Backpack(backpackMaxWeight, items));
+        }
+        return population;
+    }
+
+    public List<Backpack> newPopulation(List<Backpack> population) {
+        List<Pair> pairsToCross = new ArrayList<>();
+        if (selectionMethod == 0) {
+            for (int i = 0; i < populationSize / 2; i++) {
+                pairsToCross.add(selectPairRoulette(population));
+            }
+        } else if (selectionMethod == 1) {
+            for (int i = 0; i < populationSize / 2; i++) {
+                pairsToCross.add(selectPairTournament(population));
+            }
+        }
+        List<Backpack> nextPopulation = new ArrayList<>();
+        if (crossMethod == 0) {
+            for (Pair pair : pairsToCross) {
+                nextPopulation.addAll(pair.crossSinglePoint());
+            }
+        } else if (crossMethod == 1) {
+            for (Pair pair : pairsToCross) {
+                nextPopulation.addAll(pair.crossDoublePoint());
+            }
+        }
+        return nextPopulation;
+    }
+
+    public Pair selectPairRoulette(List<Backpack> population) {
+        return null;
+    }
+
+    public Pair selectPairTournament(List<Backpack> population) {
+        return null;
+    }
+}

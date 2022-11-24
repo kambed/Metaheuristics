03a6e8b6fcce2383a256153925ef7b05ebef86c3
Kamil Bednarek <kamil.bednarek.2001@gmail.com>
2022-11-24 20:15:28 +0100

    Added x, y to road chart

--- task3/src/main/java/pl/meta/task3/frontend/PathGenerator.java
+++ task3/src/main/java/pl/meta/task3/frontend/PathGenerator.java
@@ -17,13 +17,13 @@ public class PathGenerator extends JFrame {
                 Graphics2D graphics = (Graphics2D) g.create();
                 Path2D polyline = new Path2D.Double();
                 polyline.moveTo(x.get(0), y.get(0));
-                JLabel label = new JLabel(labels.get(0));
+                JLabel label = new JLabel(labels.get(0) + " [" + (int) Math.round(x.get(0)) + "," + (int) Math.round(y.get(0)) + "]");
                 Dimension size = label.getPreferredSize();
                 label.setBounds((int) Math.round(x.get(0)), (int) Math.round(y.get(0)), size.width, size.height);
                 f.add(label);
                 for (int i = 1; i < x.size(); i++) {
                     polyline.lineTo(x.get(i), y.get(i));
-                    label = new JLabel(labels.get(i));
+                    label = new JLabel(labels.get(i) + " [" + (int) Math.round(x.get(i)) + "," + (int) Math.round(y.get(i)) + "]");
                     size = label.getPreferredSize();
                     label.setBounds((int) Math.round(x.get(i)), (int) Math.round(y.get(i)), size.width, size.height);
                     f.add(label);
